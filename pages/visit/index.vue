580aa2f2dfd87dc2d04480fe0dee4da98a84523b
saiphp <besabellacyrus@gmail.com>
2019-10-20 18:44:44 +0800

    added pairing images

--- pages/visit/index.vue
+++ pages/visit/index.vue
@@ -1,12 +1,41 @@
 <template>
   <div>
-    visit
+    <HeroImageDynamic
+      class="pt-32"
+      :img-url="heroImage"
+    ></HeroImageDynamic>
+    <RightImageSectionDynamic
+      subtitle="CARE FOR A VISIT?"
+      title="TASTE OUR WINES"
+      message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dolore quibusdam minus? Facere repudiandae error aspernatur natus. Aliquam, tenetur eum cupiditate quae impedit possimus repellendus accusamus incidunt deleniti dignissimos corrupti quasi, perferendis dolorum explicabo a? Ipsam dolor eveniet facilis molestiae cupiditate cum magni id unde?
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et est blanditiis consequatur asperiores magni voluptatem a vero cupiditate fugiat tempora tenetur sequi omnis ipsa eligendi nostrum error officia aspernatur optio dolor, adipisci doloribus magnam?
+      "
+    >
+      <h1>Hello</h1>
+
+    </RightImageSectionDynamic>
+    <ReservationComponent></ReservationComponent>
   </div>
+
 </template>
 
 <script>
-export default {
+import HeroImageDynamic from "../../components/dynamic/HeroImageDynamic"
+import RightImageSectionDynamic from "../../components/dynamic/RightImageSectionDynamic"
+import ReservationComponent from "../../components/sections/ReservationComponent"
+const heroImage = require('../../assets/img/visit/visit-header-min.png')
 
+export default {
+  components: {
+    HeroImageDynamic,
+    RightImageSectionDynamic,
+    ReservationComponent
+  },
+  data () {
+    return {
+      heroImage
+    }
+  }
 }
 </script>
 
