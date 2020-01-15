7b866ed69ef92c12091e4b5a017a9518776aa2ee
Cyrus <besabellacyrus@gmail.com>
2020-01-16 07:30:50 +0800

    change api graphql to prod

--- apollo/config.js
+++ apollo/config.js
@@ -3,7 +3,7 @@ import cache from './cache'
 export default function (context) {
   return {
     // httpEndpoint: 'http://157.245.62.130/wp-api/graphql',
-    httpEndpoint: 'http://headless-cms.test/graphql',
+    httpEndpoint: 'http://157.245.62.130/wp-api/graphql',
     httpLinkOptions: {
       credentials: 'same-origin'
     },
