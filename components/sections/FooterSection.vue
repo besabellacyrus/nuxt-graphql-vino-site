e55c4ae29bf87279bf31f75bee04eb13ac3cfbac
saiphp <besabellacyrus@gmail.com>
2020-05-18 15:06:42 +0800

    added fixed to footer

--- components/sections/FooterSection.vue
+++ components/sections/FooterSection.vue
@@ -1,75 +1,77 @@
 <template>
   <footer>
-    <div>
-      <img
-        class="h-20 mx-auto"
-        src="~/assets/img/logo-footer.svg"
-        alt=""
-      >
-      <h2 class="mt-3"><b>HERITAGE LIQUORS</b></h2>
-    </div>
-    <div class="explores">
-      <h1>EXPLORE</h1>
-      <ul>
-        <li>
-          <a href="/">
-            HOME
-          </a>
-        </li>
-        <li>
-          <a href="/about">
-            OUR STORY
-          </a>
-        </li>
-        <li>
-          <a href="/wines">
-            WINES
-          </a>
-        </li>
-        <li>
-          <a href="/pairing">
-            PAIRING
-          </a>
-        </li>
-        <li>
-          <a href="/visit">
-            VISIT
-          </a>
-        </li>
-        <li>
-          <a href="/shop">
-            SHOP
-          </a>
-        </li>
-        <li>
-          <a href="our-orchard">
-            OUR ORCHARD
-          </a>
-        </li>
-        <li>
-          <a href="/store-locator">
-            STORE LOCATOR
-          </a>
-        </li>
-      </ul>
-    </div>
-    <div class="resources">
-      <h1>RESOURCES</h1>
-      <ul>
-        <li>SHIPPING</li>
-        <li>PRIVACY POLICY</li>
-        <li>SUSTAINABILITY</li>
-        <li>PARTNERSHIPS</li>
-      </ul>
-    </div>
-    <div class="contact">
-      <h1>CONTACT US</h1>
-      <p class="px-5 text-center md:text-left">
-        U2518 Corporate Ave. Corner Parkway Place, Filinvest City , Alabang, Muntinlupa, Metro Manila
-      </p>
-      <div class="get-connected">
-        <h1>GET CONNECTED</h1>
-        <SocialMedia />
+    <div class="footer-wrapper">
+      <div>
+        <img
+          class="h-20 mx-auto"
+          src="~/assets/img/logo-footer.svg"
+          alt=""
+        >
+        <h2 class="mt-3"><b>HERITAGE LIQUORS</b></h2>
+      </div>
+      <div class="explores">
+        <h1>EXPLORE</h1>
+        <ul>
+          <li>
+            <a href="/">
+              HOME
+            </a>
+          </li>
+          <li>
+            <a href="/about">
+              OUR STORY
+            </a>
+          </li>
+          <li>
+            <a href="/wines">
+              WINES
+            </a>
+          </li>
+          <li>
+            <a href="/pairing">
+              PAIRING
+            </a>
+          </li>
+          <li>
+            <a href="/visit">
+              VISIT
+            </a>
+          </li>
+          <li>
+            <a href="/shop">
+              SHOP
+            </a>
+          </li>
+          <li>
+            <a href="our-orchard">
+              OUR ORCHARD
+            </a>
+          </li>
+          <li>
+            <a href="/store-locator">
+              STORE LOCATOR
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="resources">
+        <h1>RESOURCES</h1>
+        <ul>
+          <li>SHIPPING</li>
+          <li>PRIVACY POLICY</li>
+          <li>SUSTAINABILITY</li>
+          <li>PARTNERSHIPS</li>
+        </ul>
+      </div>
+      <div class="contact">
+        <h1>CONTACT US</h1>
+        <p class="px-5 text-center md:text-left">
+          U2518 Corporate Ave. Corner Parkway Place, Filinvest City , Alabang, Muntinlupa, Metro Manila
+        </p>
+        <div class="get-connected">
+          <h1>GET CONNECTED</h1>
+          <SocialMedia />
+        </div>
       </div>
     </div>
   </footer>
@@ -117,10 +119,15 @@ li {
 }
 footer {
   background-color: $app-main-dark;
+  padding: 2rem;
+}
+.footer-wrapper {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(17rem, 1fr));
   text-align: center;
-  padding: 2rem;
+  max-width: 1440px;
+  margin: 0 auto;
+  /* padding: 2rem; */
 }
 .explores {
   @media screen and (max-width: 425px) {
