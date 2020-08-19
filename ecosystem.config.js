9f47658a1a132d8e956dd32f919868a1f5f9dfa0
sai <besabellacyrus@gmail.com>
2020-08-19 13:05:18 +0800

    fixed issue ecosystem pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   apps: [
     {
-      name: "nuxt-dev",
-      script: "npm",
-      args: "run dev"
-    },
-    {
       name: "nuxt-prod",
       script: "npm",
       args: "run start"
